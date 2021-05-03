3afd78bcb1c274d6b90b0295ca147f0f6493b0e8
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-03 16:47:01 +0800

    add type.js loading...

--- 13-hello-webpack/src/add-time.js
+++ 13-hello-webpack/src/add-time.js
@@ -1,9 +1,21 @@
+import Typed from 'typed.js';
+// const Typed = require('typed.js')
+
 import { timeoutPromise } from './utils.js';
+
+// npm install typed.js --save
 
 async function addTime(timeout) {
   const timeDiv = document.createElement('div');
-  timeDiv.textContent = 'Loading...';
   document.getElementById('container').appendChild(timeDiv);
+
+  const textspan = document.createElement('span');
+  timeDiv.appendChild(textspan);
+
+  var typed = new Typed(textspan, {
+    strings: ["Loading..."],
+    typeSpeed: 30
+  });
 
   await timeoutPromise(timeout);
 
--- 13-hello-webpack/webpack.config.js
+++ 13-hello-webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
