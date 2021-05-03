44660d0fe0a58cbd0020229eba5f7553da56ce05
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-03 14:05:14 +0800

    NodeJS interactive shell

--- 11-node-pretty-json/pretty-print-json.js
+++ 11-node-pretty-json/pretty-print-json.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises
 
+console.log(process.argv)
 const filename = process.argv[2];
 if (!filename) {
   console.error("Please provide json filename");
@@ -8,6 +9,13 @@ if (!filename) {
 
 async function main(jsonFilename) {
   // let's start coding
+  // console.log(jsonFilename)
+  const jsonContent = await fs.readFile(jsonFilename, { encoding: 'utf8'});
+  const json = JSON.parse(jsonContent)
+  process.stdout.write(JSON.stringify(json, null, 2))
 }
 
-main(filename);
+// main(filename);
+
+// to run on terminal;
+// node pretty-print-json.js posts.json
