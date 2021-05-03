8cf41dd47081c7af1600f97195214cf22579b2b2
xiao-tang <10761101@gm.nfu.edu.tw>
2021-05-03 15:18:04 +0800

    require fast.csv

--- 12-node-project-json-to-csv/main.js
+++ 12-node-project-json-to-csv/main.js
@@ -3,7 +3,7 @@ const { readFile } = require('fs').promises;
 const { createWriteStream } = require('fs');
 // https://nodejs.org/api/fs.html#fs_fs_createwritestream_path_options
 
-// const csv = require('fast-csv');
+const csv = require('fast-csv');
 // https://c2fo.io/fast-csv/docs/introduction/example#formatting
 
 const argJsonFilename = process.argv[2];
@@ -14,7 +14,19 @@ if (!(argJsonFilename && argCsvFilename)) {
 }
 
 async function main(jsonFilename, csvFilename) {
-  // let's start coding
+  // console.log(csv,jsonFilename,csvFilename)
+  const jsonContent = await readFile(jsonFilename, { encoding: 'utf8' });
+  const json = JSON.parse(jsonContent)
+
+  const csvStream = csv.format({ hearders : true })
+  const fileStream = createWriteStream(csvFilename)
+
+  csvStream.pipe(fileStream).on('end', () => process.exit());
+
+  json.forEach(post => {
+    csvStream.write(post);
+  })
+  csvStream.end();
 }
 
 main(argJsonFilename, argCsvFilename);
